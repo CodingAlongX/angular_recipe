56b43d6210b69005a21be0ca4360baeea1c99d9a
S. M. JAHANGIR <smjxpro@gmail.com>
2020-10-09 20:04:51 +0600

    adding the shopping list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,2 +1,16 @@
+import {Ingredient} from '../shared/ingredient.model';
+
 export class ShoppingListService {
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  getIngredients(): any {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient): any {
+    this.ingredients.push(ingredient);
+  }
 }
